bfa66477ed35d0dd86399918cc92e2e02c714165
Santiago Rodriguez <an.santiago@tutanota.com>
2018-03-20 22:09:46 -0500

    se agrega actualizaciondes de paquetes para corregir build

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,12 +4,12 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
-  mode: 'development',
   devServer: {
     contentBase: path.resolve(__dirname, './'),
     hot: true,
   },
   devtool: 'inline-source-map',
+  mode: 'development',
   module: {
     rules: [
       {
